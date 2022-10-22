bbbd8745f6775ddc97d90404e51854531ee3e69a
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2022-10-22 21:57:28 +0200

    change back

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 console.log("hello world");
 
 document.getElementById("theme").addEventListener("click", themeChange);
+const bodyEl = document.getElementById("body");
+const projectSection = document.getElementById("projects");
+const heroEl = document.getElementById("hero");
+const introEl = document.getElementById("intro");
 
 function themeChange() {
   console.log("button clicked");
-  const bodyEl = document.getElementById("body");
-  const projectSection = document.getElementById("projects");
-  const heroEl = document.getElementById("hero");
+
   bodyEl.classList.toggle("dark-theme");
   projectSection.classList.toggle("dark-theme");
   heroEl.classList.toggle("dark-theme");
-  heroEl.innerHTML = `<h1><span style="color:#fff">
-  Hello my name is Ágnes Kuti-Palásthy.I am a web
-  developer.</span>
-</h1>`;
+  introEl.classList.toggle("dark-theme");
 }
